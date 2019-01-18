681433f0f479e76035dbde55659ac761fdc19639
Sankar <gaurisankarj@gmail.com>
2019-01-18 16:12:19 +0530

    Patch Commit

--- Udemy/NodeTodoAPI/server/db/mongoose.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const { ObjectID } = require("mongodb");
-
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
-
-module.exports = { mongoose, ObjectID };
