5de3d584359b903be34f3801f07391e515dbed6f
Sankar <gaurisankarj@gmail.com>
2019-01-18 16:09:52 +0530

    TodoAppNode Commit

---
+++ Udemy/TodoAPINode/server/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+var userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }
+});
+
+var User = mongoose.model("User", userSchema);
+
+module.exports = { User };
